e6c5ce96f54d6510f7ded06aba975b7f87d4c7ee
EswariVidya <79461522+EswariVidya@users.noreply.github.com>
2021-03-10 12:13:21 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,13 @@ variable "location" {
   type = string
   default = "westeurope"
 }
+
+resource "random_string" "userPassword" {
+  length      = 8
+  special     = true
+  min_lower   = 1
+  min_numeric = 1
+  min_upper   = 1
+  min_special = 1
+  override_special = "$%_-#@"
+}
